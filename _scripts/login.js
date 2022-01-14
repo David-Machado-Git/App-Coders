e7caaabe4d1623c8e753cb7e99c363770165b7a6
DAVID CHRISTIAN MACHADO <david.m.cristian@gmail.com>
2022-01-13 21:09:05 -0300

    Segundo dia
    Tratamentos basicos e inciando js.

---
+++ _scripts/login.js
@@ -0,0 +1,41 @@
+
+// XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+// CARREGAMENTO SITE PRELOADER ! - ABAIXO -
+// XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+
+
+
+
+$(window).on('load', function () {
+    $('#preloader .inner').fadeOut();
+    $('#preloader').delay(350).fadeOut('slow');
+    $('body').delay(350).css({ 'overflow': 'visible' });
+})
+
+// XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+// CARREGAMENTO SITE PRELOADER ! - ACIMA -
+// XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+
+
+// XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+// VALIDAÇÃO CAMPOS ! - ABAIXO -
+// XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+
+
+
+function validaLogin() {
+
+    var user = document.querySelector('#user').value;
+    // var memoriaUm = String('admin');
+    var senha = document.querySelector('#senha').value;
+    // window.alert(`${user} ` + ` ${senha}`);
+    console.log(`${user} ` + ` ${senha}`);
+
+    if (user=='admin' & senha=='admin') {
+        window.confirm('Seja bem vindo usuário ' + ' Admin');
+    }else {
+        window.confirm('Usuário ou senha incorreto.  Digite Novamente!');
+    }
+    
+
+}
