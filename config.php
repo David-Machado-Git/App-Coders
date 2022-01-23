e0cf4ababf750b82b25f44fc47253d292898d6a4
DAVID CHRISTIAN MACHADO <david.m.cristian@gmail.com>
2022-01-23 08:07:52 -0300

    Versão 5
    Telas internas

---
+++ config.php
@@ -0,0 +1,21 @@
+<?php
+
+    $dbHost = 'Localhost';
+    $dbUsername = 'root';
+    $dbPassword = '';
+    $dbName = 'form_cadastra_unidades';
+
+    $conexao = new mysqli($dbHost,$dbUsername,$dbPassword,$dbName);
+
+
+    if($conexao->connect_errno)
+    {
+        echo "Erro";
+    }
+    else
+    {
+        echo "Conexão efetuada com sucesso!";
+    }
+
+
+?>
